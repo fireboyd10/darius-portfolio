a6dff1d83fa0ee0cee57e3f6d0d08854c3cca0af
Darius-Stefan Aniculaesei <darius.aniculaesei.st74@gmail.com>
2023-11-08 17:40:26 +0000

    contact form functional

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -1,68 +1,42 @@
-import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import "../App.css";
 
 function ContactForm() {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
-  });
+  const form = useRef();
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleSubmit = (e) => {
+  const sendEmail = (e) => {
     e.preventDefault();
-    // Here, you can handle form submission and send the data to your server or perform any desired action.
-    console.log('Form data submitted:', formData);
+
+    emailjs.sendForm('service_0x62lb9', 'template_fc71b9b', form.current, 'oQIOJEzkJcr3Q5xdU')
+      .then((result) => {
+        console.log(result.text);
+        console.log("message sent");
+      }, (error) => {
+        console.log(error.text);
+      });
+
+    // Clear the form input fields after sending the message
+    form.current.reset();
   };
 
   return (
-    <div className="d-flex justify-content-center align-items-center" style={{ height: '50vh' }}>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3" controlId="name">
-          <Form.Label>Name</Form.Label>
-          <Form.Control
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            placeholder="Your Name"
-            style={{ width: '100%' }} // Adjust the width as needed
-          />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            placeholder="Your Email"
-            style={{ width: '150vh' }} // Adjust the width as needed
-          />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="message">
-          <Form.Label>Message</Form.Label>
-          <Form.Control
-            as="textarea"
-            rows={4}
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
-            placeholder="Your Message"
-            style={{ width: '150vh' }} // Adjust the width as needed
-          />
-        </Form.Group>
-
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+    <div className="form-container">
+      <form ref={form} onSubmit={sendEmail}>
+        <div className="form-group">
+          <label>Name</label>
+          <input type="text" name="user_name" />
+        </div>
+        <div className="form-group">
+          <label>Email</label>
+          <input type="email" name="user_email" />
+        </div>
+        <div className="form-group">
+          <label>Message</label>
+          <textarea name="message" />
+        </div>
+        <button type="submit" className="blue-button">Send</button>
+      </form>
     </div>
   );
 }
